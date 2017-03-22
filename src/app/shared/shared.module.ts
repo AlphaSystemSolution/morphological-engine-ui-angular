31ba8892e5f0796170782171e8832301f1c6dd25
sfali23 <f.syed.ali@gmail.com>
2017-03-22 07:51:11 -0400

    using rtl style to display

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { OverlayPanelModule } from 'primeng/primeng';
+import { OverlayPanelModule, ButtonModule } from 'primeng/primeng';
 import { ArabicButtonComponent } from './arabic-button/arabic-button.component';
 import { ArabicKeyboardComponent } from './arabic-keyboard/arabic-keyboard.component';
 
 @NgModule({
   imports: [
-    CommonModule, OverlayPanelModule
+    CommonModule, ButtonModule, OverlayPanelModule
   ],
   exports: [ArabicButtonComponent, ArabicKeyboardComponent],
   declarations: [ArabicButtonComponent, ArabicKeyboardComponent]
