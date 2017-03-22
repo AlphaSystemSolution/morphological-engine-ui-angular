cc6711c8d6a9997dd1616b365a8e5751587de900
sfali23 <f.syed.ali@gmail.com>
2017-03-22 07:17:22 -0400

    initial testing version for keyboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { ArabicKeyboardComponent } from './shared/arabic-keyboard/arabic-keyboard.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+  title = 'Morphological Engine!';
+
+  ngOnInit() {
+  }
+
 }
