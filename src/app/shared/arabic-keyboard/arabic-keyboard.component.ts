8081e06f282c91c7af7efd176beec6fe54384aa4
sfali23 <f.syed.ali@gmail.com>
2017-03-22 09:23:26 -0400

    adding event listener

--- src/app/shared/arabic-keyboard/arabic-keyboard.component.ts
+++ src/app/shared/arabic-keyboard/arabic-keyboard.component.ts
@@ -13,18 +13,41 @@ import { ArabicLetter, arabicLetters } from '../model';
 export class ArabicKeyboardComponent implements OnInit {
 
   @ViewChild('rootLettersPicker') rootLettersPicker: OverlayPanel;
+  private _selectedLetters: ArabicLetter[];
+  private currentIndex: number;
 
   constructor() { }
 
   ngOnInit() {
+    this._selectedLetters = [];
+    this.resetSelection();
   }
 
-  @Input() get letters(): ArabicLetter[] {
+  get letters(): ArabicLetter[] {
     return arabicLetters;
+  }
+
+  get selectedLetters(): ArabicLetter[] {
+    return this._selectedLetters;
   }
 
   show(event): void {
     this.rootLettersPicker.show(event);
+  }
+
+  selectLetter(letter: ArabicLetter): void {
+    // update this letter @ currentIndex
+    this._selectedLetters[this.currentIndex] = letter;
+    // update currentIndex
+    this.currentIndex = (this.currentIndex + 1) % 4;
+  }
+
+  resetSelection(): void {
+    this.currentIndex = 0;
+    this._selectedLetters[0] = this.letters[19];
+    this._selectedLetters[1] = this.letters[17];
+    this._selectedLetters[2] = this.letters[22];
+    this._selectedLetters[3] = null;
   }
 
 }
