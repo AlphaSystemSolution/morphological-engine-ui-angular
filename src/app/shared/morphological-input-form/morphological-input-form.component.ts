87ae2066fdc946f89cdb37d6c81c3f02ee5c749b
sfali <sali2@loyalty.com>
2017-03-24 15:52:18 -0400

    adding fields "removePassiveLine" and "skipRuleProcessing"

--- src/app/shared/morphological-input-form/morphological-input-form.component.ts
+++ src/app/shared/morphological-input-form/morphological-input-form.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { ArabicKeyboardComponent } from '../arabic-keyboard/arabic-keyboard.component';
+import { ArabicDropdownComponent } from '../arabic-dropdown/arabic-dropdown.component';
+import {
+  ArabicLetter, RootLetters, DisplayType, NamedTemplate, MorphologicalInput, ConjugationConfiguration, arabicLetters,
+  namedTemplates, defaultMorphologicalInput
+} from '../model';
 
 @Component({
   selector: 'app-morphological-input-form',
@@ -7,9 +14,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MorphologicalInputFormComponent implements OnInit {
 
-  constructor() { }
+  private _mInput: MorphologicalInput;
+
+  private _removePassiveLine: boolean;
+  private _skipRuleProcessing: boolean;
+
+  // morphological Input Selection (MIS) form
+  misForm: FormGroup;
+
+  constructor(fb: FormBuilder) {
+    this.misForm = fb.group({
+      'removePassiveLine': new FormControl(),
+      'skipRuleProcessing': new FormControl()
+    });
+  }
 
   ngOnInit() {
+  }
+
+  @Input() get mInput(): MorphologicalInput {
+    if (this._mInput === null) {
+      this.mInput = null;
+    }
+    return this._mInput;
+  }
+
+  set mInput(src: MorphologicalInput) {
+    if (!src) {
+      src = defaultMorphologicalInput;
+    }
+    this._mInput = src;
+    const conjugationConfiguration: ConjugationConfiguration = this.mInput.conjugationConfiguration;
+    this.removePassiveLine = conjugationConfiguration.removePassiveLine;
+    this.skipRuleProcessing = conjugationConfiguration.skipRuleProcessing;
+  }
+
+  get removePassiveLine(): boolean {
+    return this._removePassiveLine;
+  }
+
+  set removePassiveLine(value: boolean) {
+    this._removePassiveLine = value;
+    this._mInput.conjugationConfiguration.removePassiveLine = this._removePassiveLine;
+  }
+
+  get skipRuleProcessing(): boolean {
+    return this._skipRuleProcessing;
+  }
+
+  set skipRuleProcessing(value: boolean) {
+    this._skipRuleProcessing = value;
+    this._mInput.conjugationConfiguration.skipRuleProcessing = this._skipRuleProcessing;
   }
 
 }
