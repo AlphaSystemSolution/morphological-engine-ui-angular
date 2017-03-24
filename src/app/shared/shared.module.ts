a8808526b453d2e9c08761be6296128b85e9ebbc
sfali <sali2@loyalty.com>
2017-03-24 15:50:49 -0400

    format

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,6 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonModule, OverlayPanelModule, TemplateWrapper, ToggleButtonModule } from 'primeng/primeng';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {
+  ButtonModule,
+  InputSwitchModule,
+  OverlayPanelModule,
+  PanelModule,
+  TemplateWrapper,
+  ToggleButtonModule
+} from 'primeng/primeng';
 import { ArabicButtonComponent } from './arabic-button/arabic-button.component';
 import { ArabicKeyboardComponent } from './arabic-keyboard/arabic-keyboard.component';
 import { ArabicDropdownComponent } from './arabic-dropdown/arabic-dropdown.component';
@@ -8,9 +16,25 @@ import { MorphologicalInputFormComponent } from './morphological-input-form/morp
 
 @NgModule({
   imports: [
-    CommonModule, ButtonModule, OverlayPanelModule, ToggleButtonModule
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    ButtonModule,
+    InputSwitchModule,
+    OverlayPanelModule,
+    PanelModule,
+    ToggleButtonModule
   ],
-  exports: [ArabicButtonComponent, ArabicKeyboardComponent, ArabicDropdownComponent, TemplateWrapper, MorphologicalInputFormComponent],
-  declarations: [ArabicButtonComponent, ArabicKeyboardComponent, ArabicDropdownComponent, TemplateWrapper, MorphologicalInputFormComponent]
+  exports: [
+    ArabicButtonComponent,
+    ArabicKeyboardComponent,
+    ArabicDropdownComponent,
+    TemplateWrapper,
+    MorphologicalInputFormComponent],
+  declarations: [
+    ArabicButtonComponent,
+    ArabicKeyboardComponent,
+    ArabicDropdownComponent,
+    MorphologicalInputFormComponent]
 })
 export class SharedModule { }
