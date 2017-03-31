11d63a0543909e2dbc2c36aa0cb393ac7353de3c
sfali23 <f.syed.ali@gmil.com>
2017-03-31 14:58:02 -0400

    making changes to be used in dialog to add / update values

--- src/app/components/morphological-input-form/morphological-input-form.component.ts
+++ src/app/components/morphological-input-form/morphological-input-form.component.ts
@@ -1,8 +1,6 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
 import { MorphologicalInputFormModel } from '../../shared/morphological-input-form-model';
-import { ApplicationControllerService } from '../../application-controller.service';
 import { ArabicKeyboardComponent } from '../../shared/arabic-keyboard/arabic-keyboard.component';
 import { ArabicDropdownComponent } from '../../shared/arabic-dropdown/arabic-dropdown.component';
 import { VerbalNounPickerComponent } from '../../shared/verbal-noun-picker/verbal-noun-picker.component';
@@ -16,10 +14,13 @@ import { NamedTemplate, namedTemplates } from '../../shared/model';
 export class MorphologicalInputFormComponent implements OnInit {
 
   @ViewChild('verbalNounPicker') verbalNounPicker: VerbalNounPickerComponent;
+  private _model: MorphologicalInputFormModel;
+  mode = 'ADD';
   // morphological Input Selection (MIS) form
   misForm: FormGroup;
 
-  constructor(fb: FormBuilder, private applicationController: ApplicationControllerService, private router: Router) {
+  constructor(fb: FormBuilder) {
+    this._model = new MorphologicalInputFormModel();
     this.misForm = fb.group({
       'rootLettersText': new FormControl(),
       'template': new FormControl(),
@@ -33,23 +34,24 @@ export class MorphologicalInputFormComponent implements OnInit {
   ngOnInit() {
   }
 
+  @Input() get model(): MorphologicalInputFormModel {
+    return this._model;
+  }
+
+  set model(value: MorphologicalInputFormModel) {
+    this._model = value;
+  }
+
   get namedTemplates(): NamedTemplate[] {
     return namedTemplates;
   }
 
   onSubmit(event) {
-    this.applicationController.getMorphologicalChart();
-    this.router.navigate(['staging']).then(() => {
-      this.router.navigate(['morphological-chart']);
-    });
+    // TODO:
   }
 
   handleClose(event) {
-    this.applicationController.model.rootLetters = event.rootLetters;
-  }
-
-  viewDictionary() {
-    this.applicationController.openWithRootLetters(this.applicationController.model.mInput.rootLetters);
+    this.model.rootLetters = event.rootLetters;
   }
 
   handleSelectTemplate(event) {
@@ -62,7 +64,7 @@ export class MorphologicalInputFormComponent implements OnInit {
     event.forEach(e => {
       verbalNouns[index++] = e.value;
     });
-    this.applicationController.model.verbalNouns = verbalNouns;
+    this.model.verbalNouns = verbalNouns;
   }
 
 }
