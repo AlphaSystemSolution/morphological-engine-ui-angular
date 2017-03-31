545d976739ee424ece1e12e0f1d9a7b86a4bea6e
sfali23 <f.syed.ali@gmil.com>
2017-03-31 07:41:29 -0400

    export chart to MS Word

--- src/app/application-controller.service.ts
+++ src/app/application-controller.service.ts
@@ -7,6 +7,7 @@ import { RootLetters, MorphologicalInput } from './shared/model';
 import { MorphologicalChart } from './components/model';
 import { environment } from '../environments/environment';
 import 'rxjs/add/operator/map';
+ import 'rxjs/Rx' ;
 
 @Injectable()
 export class ApplicationControllerService {
@@ -50,6 +51,35 @@ export class ApplicationControllerService {
 
     const options = new RequestOptions({ headers: headers });
     this.http.get(url, options).map(resp => resp.json()).subscribe(this._morphologicalChartSubject);
+  }
+
+  exportMorphologicalChart() {
+    const m: MorphologicalInput = this.model.mInput;
+    const rl: RootLetters = m.rootLetters;
+    const path = 'morphologicalChart2/form/' + m.template.name + '/firstRadical/' + rl.firstRadical.name +
+      '/secondRadical/' + rl.secondRadical.name + '/thirdRadical/' + rl.thirdRadical.name + '/';
+    const url = environment.morphologicalEngineBaseUrl + path;
+    console.log(url);
+    const headers = new Headers();
+    headers.set('Content-Type', 'application/json;charset=UTF-8');
+    headers.set('format', 'UNICODE');
+    headers.set('translation', m.translation);
+    const verbalNounValues: string[] = [];
+    const verbalNouns = this.model.mInput.verbalNouns;
+    let index = 0;
+    verbalNouns.forEach(vn => verbalNounValues[index++] = vn.name);
+    headers.set('verbalNouns', verbalNounValues);
+    const conjugationConfiguration = m.conjugationConfiguration;
+    if (conjugationConfiguration) {
+      headers.set('removePassiveLine', '' + conjugationConfiguration.removePassiveLine);
+      headers.set('skipRuleProcessing', '' + conjugationConfiguration.skipRuleProcessing);
+    }
+    const options = new RequestOptions({ headers: headers });
+    this.http.get(url, options).subscribe(data => {
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' });
+      const url2 = window.URL.createObjectURL(blob);
+      window.open(url2, 'export');
+    });
   }
 
   openWithRootLetters(rootLetters: RootLetters): void {
