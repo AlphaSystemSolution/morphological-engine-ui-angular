c8c590cfc5d0cca819d8a9311191d210fe14b3b3
sfali23 <f.syed.ali@gmil.com>
2017-04-01 16:53:14 -0400

    clean up

--- src/app/application-controller.service.ts
+++ src/app/application-controller.service.ts
@@ -7,43 +7,35 @@ import { RootLetters, MorphologicalInput } from './shared/model';
 import { MorphologicalChart } from './components/model';
 import { environment } from '../environments/environment';
 import 'rxjs/add/operator/map';
- import 'rxjs/Rx' ;
 
 @Injectable()
 export class ApplicationControllerService {
 
   private _morphologicalChartSubject: BehaviorSubject<MorphologicalChart[]>;
   public morphologicalCharts: Observable<MorphologicalChart[]>;
-  private _model: MorphologicalInputFormModel;
 
   constructor(private http: Http) {
-    this._model = new MorphologicalInputFormModel();
   }
 
-  get model(): MorphologicalInputFormModel {
-    return this._model;
-  }
-
-  getMorphologicalChart(): void {
+  getMorphologicalChart(input: MorphologicalInput): void {
     this._morphologicalChartSubject = new BehaviorSubject<MorphologicalChart[]>([]);
     this.morphologicalCharts = this._morphologicalChartSubject.asObservable();
 
-    const m: MorphologicalInput = this.model.mInput;
-    const rl: RootLetters = m.rootLetters;
-    const path = 'morphologicalChart/form/' + m.template.name + '/firstRadical/' + rl.firstRadical.name +
+    const rl: RootLetters = input.rootLetters;
+    const path = 'morphologicalChart/form/' + input.template.name + '/firstRadical/' + rl.firstRadical.name +
       '/secondRadical/' + rl.secondRadical.name + '/thirdRadical/' + rl.thirdRadical.name + '/';
     const url = environment.morphologicalEngineBaseUrl + path;
     console.log(url);
     const headers = new Headers();
     headers.set('Content-Type', 'application/json;charset=UTF-8');
     headers.set('format', 'UNICODE');
-    headers.set('translation', m.translation);
+    headers.set('translation', input.translation);
     const verbalNounValues: string[] = [];
-    const verbalNouns = this.model.mInput.verbalNouns;
+    const verbalNouns = input.verbalNouns;
     let index = 0;
     verbalNouns.forEach(vn => verbalNounValues[index++] = vn.name);
     headers.set('verbalNouns', verbalNounValues);
-    const conjugationConfiguration = m.conjugationConfiguration;
+    const conjugationConfiguration = input.conjugationConfiguration;
     if (conjugationConfiguration) {
       headers.set('removePassiveLine', '' + conjugationConfiguration.removePassiveLine);
       headers.set('skipRuleProcessing', '' + conjugationConfiguration.skipRuleProcessing);
@@ -53,23 +45,22 @@ export class ApplicationControllerService {
     this.http.get(url, options).map(resp => resp.json()).subscribe(this._morphologicalChartSubject);
   }
 
-  exportMorphologicalChart() {
-    const m: MorphologicalInput = this.model.mInput;
-    const rl: RootLetters = m.rootLetters;
-    const path = 'morphologicalChart2/form/' + m.template.name + '/firstRadical/' + rl.firstRadical.name +
+  exportMorphologicalChart(input: MorphologicalInput) {
+    const rl: RootLetters = input.rootLetters;
+    const path = 'morphologicalChart2/form/' + input.template.name + '/firstRadical/' + rl.firstRadical.name +
       '/secondRadical/' + rl.secondRadical.name + '/thirdRadical/' + rl.thirdRadical.name + '/';
     const url = environment.morphologicalEngineBaseUrl + path;
     console.log(url);
     const headers = new Headers();
     headers.set('Content-Type', 'application/json;charset=UTF-8');
     headers.set('format', 'UNICODE');
-    headers.set('translation', m.translation);
+    headers.set('translation', input.translation);
     const verbalNounValues: string[] = [];
-    const verbalNouns = this.model.mInput.verbalNouns;
+    const verbalNouns = input.verbalNouns;
     let index = 0;
     verbalNouns.forEach(vn => verbalNounValues[index++] = vn.name);
     headers.set('verbalNouns', verbalNounValues);
-    const conjugationConfiguration = m.conjugationConfiguration;
+    const conjugationConfiguration = input.conjugationConfiguration;
     if (conjugationConfiguration) {
       headers.set('removePassiveLine', '' + conjugationConfiguration.removePassiveLine);
       headers.set('skipRuleProcessing', '' + conjugationConfiguration.skipRuleProcessing);
