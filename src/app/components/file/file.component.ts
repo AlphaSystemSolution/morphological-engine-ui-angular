d674d1eea3fa3e7496c85d73973341e5a401ac78
sfali23 <f.syed.ali@gmil.com>
2017-04-04 21:05:55 -0400

    implementing file import

--- src/app/components/file/file.component.ts
+++ src/app/components/file/file.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-file',
@@ -7,9 +7,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FileComponent implements OnInit {
 
+  @Input() importDialog: boolean;
+  @Output() onClose: EventEmitter<any> = new EventEmitter();
+  private file: File;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onChooseClick(event, fileInput) {
+    this.file = null;
+    fileInput.value = null;
+    fileInput.click();
+  }
+
+  onFileSelect(event, input) {
+    input.value = '';
+    const files = event.dataTransfer ? event.dataTransfer.files : event.target.files;
+    if (files && files.length > 0) {
+      this.file = files[0];
+      console.log('FILE: ' + this.file.name);
+      input.value = this.file.name;
+    }
+  }
+
+  hideDialog(event, action) {
+    this.importDialog = false;
+    let result: File = null;
+    if ('submit' === action) {
+      result = this.file;
+    }
+    this.file = null;
+    this.onClose.emit({ originalEvent: event, file: result });
   }
 
 }
