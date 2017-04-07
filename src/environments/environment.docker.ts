a97c170e960e1786f5326f38b3df4a7f440d6661
sfali23 <f.syed.ali@gmil.com>
2017-04-07 08:43:58 -0400

    change port

--- src/environments/environment.docker.ts
+++ src/environments/environment.docker.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   dictionaryUrl: 'http://ejtaal.net/aa/index.html#bwq=',
-  morphologicalEngineBaseUrl: 'http://192.168.99.100:8080/morphologicalEngine/'
+  morphologicalEngineBaseUrl: 'http://192.168.99.100:15750/morphologicalEngine/'
 };
