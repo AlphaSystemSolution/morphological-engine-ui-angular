7aee755fe67d13af99c10dd893b6d5530be2a9f1
sfali23 <f.syed.ali@gmil.com>
2017-04-25 21:25:50 -0400

    updated model

--- src/app/model/detailed-conjugation.ts
+++ src/app/model/detailed-conjugation.ts
@@ -1,6 +1,18 @@
 export class ConjugationTuple {
   private _type: string;
-  constructor(public singular: string, public dual: string, public plural: string) { }
+  public singular: string;
+  public dual: string;
+  public plural: string;
+
+  static getTuple(value: any): ConjugationTuple {
+    return value ? new ConjugationTuple(value) : null;
+  }
+
+  constructor(src?: any) {
+    this.singular = src && src.singular || null;
+    this.dual = src && src.dual || null;
+    this.plural = src && src.plural || null;
+  }
 
   get type(): string {
     return this._type;
@@ -16,13 +28,35 @@ export interface ConjugationGroup {
 }
 
 export class VerbConjugationGroup implements ConjugationGroup {
-  constructor(public termType: string, public masculineThirdPerson: ConjugationTuple, public feminineThirdPerson: ConjugationTuple,
-    public masculineSecondPerson: ConjugationTuple, public feminineSecondPerson: ConjugationTuple, public firstPerson: ConjugationTuple) { }
+  public termType: string;
+  public masculineThirdPerson: ConjugationTuple;
+  public feminineThirdPerson: ConjugationTuple;
+  public masculineSecondPerson: ConjugationTuple;
+  public feminineSecondPerson: ConjugationTuple;
+  public firstPerson: ConjugationTuple;
+
+  constructor(src?: any) {
+    this.termType = src && src.termType || null;
+    this.masculineThirdPerson = ConjugationTuple.getTuple(src && src.masculineThirdPerson || null);
+    this.feminineThirdPerson = ConjugationTuple.getTuple(src && src.feminineThirdPerson || null);
+    this.masculineSecondPerson = ConjugationTuple.getTuple(src && src.masculineSecondPerson || null);
+    this.feminineSecondPerson = ConjugationTuple.getTuple(src && src.feminineSecondPerson || null);
+    this.firstPerson = ConjugationTuple.getTuple(src && src.firstPerson || null);
+  }
 }
 
 export class NounConjugationGroup implements ConjugationGroup {
-  constructor(public termType: string, public nominative: ConjugationTuple, public accusative: ConjugationTuple,
-    public genitive: ConjugationTuple) { }
+  public termType: string;
+  public nominative: ConjugationTuple;
+  public accusative: ConjugationTuple;
+  public genitive: ConjugationTuple;
+
+  constructor(src?: any) {
+    this.termType = src && src.termType || null;
+    this.nominative = ConjugationTuple.getTuple(src && src.nominative || null);
+    this.accusative = ConjugationTuple.getTuple(src && src.accusative || null);
+    this.genitive = ConjugationTuple.getTuple(src && src.genitive || null);
+  }
 }
 
 export class DetailedConjugation {
@@ -47,4 +81,37 @@ export class DetailedConjugation {
 
   // adverb values
   public adverbs: NounConjugationGroup[];
+
+  private static getVerbConjugationGroup(src: any): VerbConjugationGroup {
+    return src ? new VerbConjugationGroup(src) : null;
+  }
+
+  private static getNounConjugationGroup(src: any): NounConjugationGroup {
+    return src ? new NounConjugationGroup(src) : null;
+  }
+
+  constructor(src?: any) {
+    this.pastTense = DetailedConjugation.getVerbConjugationGroup(src && src.pastTense || null);
+    this.presentTense = DetailedConjugation.getVerbConjugationGroup(src && src.presentTense || null);
+    this.activeParticipleMasculine = DetailedConjugation.getNounConjugationGroup(src && src.activeParticipleMasculine || null);
+    this.activeParticipleFeminine = DetailedConjugation.getNounConjugationGroup(src && src.activeParticipleFeminine || null);
+
+    this.pastPassiveTense = DetailedConjugation.getVerbConjugationGroup(src && src.pastPassiveTense || null);
+    this.pastPassiveTense = DetailedConjugation.getVerbConjugationGroup(src && src.pastPassiveTense || null);
+    this.passiveParticipleMasculine = DetailedConjugation.getNounConjugationGroup(src && src.passiveParticipleMasculine || null);
+    this.passiveParticipleFeminine = DetailedConjugation.getNounConjugationGroup(src && src.passiveParticipleFeminine || null);
+
+    this.imperative = DetailedConjugation.getVerbConjugationGroup(src && src.imperative || null);
+    this.forbidding = DetailedConjugation.getVerbConjugationGroup(src && src.forbidding || null);
+
+    if (src && src.verbalNouns && src.verbalNouns.length > 0) {
+      this.verbalNouns = [];
+      src.verbalNouns.foreach(value => this.verbalNouns.push(DetailedConjugation.getNounConjugationGroup(value)));
+    }
+
+    if (src && src.adverbs && src.adverbs.length > 0) {
+      this.adverbs = [];
+      src.adverbs.foreach(value => this.adverbs.push(DetailedConjugation.getNounConjugationGroup(value)));
+    }
+  }
 }
