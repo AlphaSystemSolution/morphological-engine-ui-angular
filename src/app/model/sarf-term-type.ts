574844b2432bbf33c21b6d752aac355639d58784
sfali23 <f.syed.ali@gmil.com>
2017-04-27 07:30:34 -0400

    find type by name

--- src/app/model/sarf-term-type.ts
+++ src/app/model/sarf-term-type.ts
@@ -62,4 +62,10 @@ export class SarfTermType extends ArabicLabel {
     super(name, label, null);
     this.code = this.name;
   }
+
+  get isVerbType(): boolean {
+    return this.name === SarfTermType.PAST_TENSE.name || this.name === SarfTermType.PRESENT_TENSE.name
+      || this.name === SarfTermType.PAST_PASSIVE_TENSE.name || this.name === SarfTermType.PRESENT_PASSIVE_TENSE.name
+      || this.name === SarfTermType.IMPERATIVE.name || this.name === SarfTermType.FORBIDDING.name;
+  }
 }
