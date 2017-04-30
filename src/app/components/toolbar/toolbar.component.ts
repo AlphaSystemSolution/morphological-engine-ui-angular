2dbf5620f287d062d7c09cfe52aa12537f8b9c8d
sfali23 <f.syed.ali@gmil.com>
2017-04-30 16:52:44 -0400

    removing as it is moved to context menu

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -8,8 +8,6 @@ import { MenuItem } from 'primeng/primeng';
 export class ToolbarComponent implements OnInit {
 
   fileItems: MenuItem[];
-  actionItems: MenuItem[];
-  viewItems: MenuItem[];
   @Output() onAction: EventEmitter<string> = new EventEmitter();
   constructor() { }
 
@@ -18,15 +16,6 @@ export class ToolbarComponent implements OnInit {
       // { label: 'New', icon: 'fa-file', command: () => this.handleButtonClick('NEW') },
       { label: 'Import', icon: 'fa-download', command: () => this.handleButtonClick('IMPORT') },
       { label: 'Export', icon: 'fa-upload', command: () => this.handleButtonClick('EXPORT') }
-    ];
-    this.actionItems = [
-      { label: 'Add', icon: 'fa-plus-square', command: () => this.handleButtonClick('ADD') },
-      { label: 'Edit', icon: 'fa-pencil-square', command: () => this.handleButtonClick('EDIT') },
-      { label: 'Duplicate', icon: 'fa-clone', command: () => this.handleButtonClick('DUPLICATE') },
-      { label: 'Remove', icon: 'fa-times', command: () => this.handleButtonClick('REMOVE') }
-    ];
-    this.viewItems = [
-      { label: 'Dictionary', command: () => this.handleButtonClick('DICTIONARY') }
     ];
   }
 
