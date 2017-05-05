4a07f367a2241443e5aa35335c52c8d5e3bc726a
sfali23 <f.syed.ali@gmil.com>
2017-05-04 21:43:51 -0400

    New Project dialog component

--- src/app/components/new-project/new-project.component.ts
+++ src/app/components/new-project/new-project.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { Project } from '../../model/project';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/fromEvent';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-new-project',
@@ -7,9 +13,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewProjectComponent implements OnInit {
 
+  @ViewChild('projectName') projectNameInput: ElementRef;
+  @Input() visible: boolean;
+  @Output() onClose: EventEmitter<any> = new EventEmitter();
+  project: Project;
+  private _inactive = true;
+
   constructor() { }
 
   ngOnInit() {
+    this.project = new Project();
+    Observable.fromEvent(this.projectNameInput.nativeElement, 'keyup')
+      .map((e: any) => e.target.value) // extract the value of the input
+      .filter((text: string) => {
+        this.inactive = text.length <= 1;
+        return !this.inactive;
+      }) // filter out if empty
+      .debounceTime(500) // only once every 500ms
+      .subscribe(
+      (text: string) => this.project.fileName = text.replace(/ /g, '_').toLocaleLowerCase() + '.json',
+      (err) => {
+        console.log('------------ ' + JSON.stringify(err));
+      }
+      );
+  }
+
+  get inactive(): boolean {
+    return this._inactive;
+  }
+
+  set inactive(value: boolean) {
+    this._inactive = value;
+    if (this.inactive) {
+      this.project.fileName = '';
+    }
+  }
+
+  submit(event, action: string) {
+    this.visible = false;
+    if ('submit' === action) {
+      this.onClose.emit({ originalEvent: event, result: this.project });
+    } else {
+      this.onClose.emit({ originalEvent: event, result: undefined });
+    }
+    this.project = new Project();
   }
 
 }
